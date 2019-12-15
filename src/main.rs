127c056cba88a7d5326992e83709b1fbdb3fab19
thomas hryckewycz <thryckz@Live.com>
2019-12-15 14:23:46 -0500

    wages can now preform the wage calculation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+mod wages;
+
 fn main() {
-    println!("Hello, world!");
+    loop{
+        let term = console::Term::stdout();
+        let title = "Wages";
+        let description = "A simple wage calculator written in rust";
+        let author = "tavichh";
+    
+        term.set_title(title);
+    
+        println!("{}",description);
+        println!("Created by {}",author);
+        println!("", );
+    
+        println!("Please enter your hourly wage.", );
+        let wage = term.read_line()
+            .unwrap()
+            .parse::<f32>();
+    
+        println!("Please enter how many hours you worked.");
+        let hours = term.read_line()
+            .unwrap()
+            .parse::<f32>();
+    
+        println!("You have made: ${}",
+            wages::evaluate(wage.unwrap(), 
+            hours.unwrap()));
+    
+        println!("Press a key to preform another calculation.");
+        term.read_key().ok();
+    }
 }
---
+++ src/wages.rs
@@ -0,0 +1,5 @@
+pub fn evaluate(u_wage: f32, 
+    u_hour: f32) -> f32 {
+        let total = u_hour * u_wage;
+        total
+}
